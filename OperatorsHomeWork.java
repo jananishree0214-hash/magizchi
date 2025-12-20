c0df38a0f481013c329d41fcb7387a864dde287b
jananishree0214-hash <jananishree0214@gmail.com>
2025-12-21 02:34:55 +0530

    Add files via upload

---
+++ OperatorsHomeWork.java
@@ -0,0 +1,29 @@
+class OperatorsHomeWork
+
+{
+   public static void main (String[] args)
+
+   {
+
+          int x = 10;
+          int y = 15;
+     
+
+    System.out.println("X==Y:"+(x==y));
+    System.out.println("X !=Y :"+(x !=y));
+    System.out.println("X > Y : "+(x > y));
+    System.out.println("X < Y : "+(x < y));
+    System.out.println("X >=Y:"+(x >= y));
+    System.out.println("X <=Y:"+(x <=y));
+    
+ 
+
+   }
+
+
+
+
+
+
+
+}
---
+++ Scholarship.java
@@ -0,0 +1,26 @@
+class Scholarship
+
+{
+   public static void main (String[] args)
+
+   {
+
+          int marks = 85;
+          boolean attendance = true;
+          boolean discipline = true;
+
+         boolean result = (marks >=60 )&&attendance && discipline;
+     
+
+    System.out.println("pass & eligible for scholarship:"+result);
+     
+
+   }
+
+
+
+
+
+
+
+}
