b971ac1708350784957ba1227980350bb2213d62
jananishree0214-hash <jananishree0214@gmail.com>
2025-12-21 02:53:23 +0530

    Add files via upload

---
+++ Operators.java
@@ -0,0 +1,27 @@
+class Operators
+
+{
+   public static void main (String[] args)
+
+   {
+
+          int a = 20;
+          int b = 6;
+     
+
+    System.out.println("Addition = "+(a+b));
+    System.out.println("Subraction = "+(a-b));
+    System.out.println("Division = "+(a/b));
+    System.out.println("Remainder = "+(a%b));
+    
+ 
+
+   }
+
+
+
+
+
+
+
+}
