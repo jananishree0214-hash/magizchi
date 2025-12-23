b818d5bcb1fe89a01262dea0b0bcc1f48dd41232
jananishree0214-hash <jananishree0214@gmail.com>
2025-12-23 21:47:43 +0530

    Add files via upload

---
+++ EvenOdd.java
@@ -0,0 +1,22 @@
+class EvenOdd 
+{
+    public static void main(String[] args) 
+  {
+
+        int num1 = 26;
+        int num2 = 35;
+
+        if (num1 % 2 == 0)
+            System.out.println(num1+ " is an even number");
+
+        else
+            System.out.println(num1 + " is an odd number");
+
+
+        if (num2 % 2 == 0)
+            System.out.println(num2 + " is an even number");
+
+        else
+            System.out.println(num2 + " is an odd number");
+    }
+}
---
+++ LargestNumber.java
@@ -0,0 +1,25 @@
+class LargestNumber
+{
+    public static void main(String[] args) 
+  {
+
+        int a= 15;
+        int b = 38;
+        
+        int c = 20;
+        int d = 13;
+
+        if (a>b)
+            System.out.println(a + "is a largest number");
+    
+        else
+            System.out.println(b + "is a largest number");
+
+
+        if (c>d)
+            System.out.println(c + "is a largest number");
+
+        else
+            System.out.println(d + "is a largest number");
+    }
+}
