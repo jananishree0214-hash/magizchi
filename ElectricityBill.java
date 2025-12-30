2edab155386f3a1e226d712d81ea9874b62bf407
jananishree0214-hash <jananishree0214@gmail.com>
2025-12-30 13:14:42 +0530

    Add files via upload

---
+++ ElectricityBill.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+class ElectricityBill 
+{
+    public static void main(String[] args) 
+{
+
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter units : ");
+        int units = sc.nextInt();
+
+        int bill;
+
+        if (units <= 100) 
+{
+            bill = units * 5;
+        } else if (units <= 200) 
+{
+            bill = units * 7;
+        } else {
+            bill = units * 10;
+        }
+
+        System.out.println("Electricity Bill: ₹" + bill);
+    }
+}
