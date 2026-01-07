78633a6d3cab9bf7f283159e2376459e81ecb81c
jananishree0214-hash <jananishree0214@gmail.com>
2026-01-07 15:36:15 +0530

    Add files via upload

---
+++ Marks.java
@@ -0,0 +1,32 @@
+class Marks
+{
+   public static void main(String[] args)
+   {
+	int ar[][] = {{85,78,90},{88,92,80},{75,75,68}};  
+	
+	for(int i = 0; i<3; i++)
+	{
+     for(int j = 0; j<3; j++)
+	 {
+		 System.out.print(ar[i][j] + " ");
+	 }
+	
+	     System.out.println();
+	
+	  
+	 
+	  
+	  
+	  
+	  
+     
+	}  
+	  
+   }
+	
+	
+	
+	
+	
+	
+}
