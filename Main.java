2aad97ccdb3c37ba38b6a3c6d80dd244a67216af
jananishree0214-hash <jananishree0214@gmail.com>
2026-01-29 14:34:13 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main
+{
+    public static void main(String[] arguments)
+    {
+        Students ravi = new Students();
+        ravi.id = 101;
+        ravi.name = "Ravi Kumar";
+        ravi.mobile = 9952810451L;
+        ravi.display();
+    }
+}
---
+++ Students.java
@@ -0,0 +1,13 @@
+class Students
+{
+    int id;
+    String name;
+    long mobile;
+
+    void display()
+    {
+        System.out.println("ID: " + id);
+        System.out.println("Name: " + name);
+        System.out.println("Mobile: " + mobile);
+    }
+}
