4c3c5b7299aa0c27cf26df45fd0f5a8d5ae94b4f
jananishree0214-hash <jananishree0214@gmail.com>
2026-02-06 14:06:44 +0530

    Add files via upload

---
+++ Person.java
@@ -0,0 +1,26 @@
+public class Person 
+{
+    private String name = "Geeks";
+    private int age = 10;
+
+    public String getName() 
+    {
+        return name;
+    }
+
+    public void setName(String name) 
+    {
+        this.name = name;
+    }
+
+    public int getAge() 
+    {
+        return age;
+    }
+
+    public void setAge(int age) 
+    {
+        this.age = age;
+    }
+    
+}
