7ed24bead3897f6465f61d0a5ad7bb58f2ecd144
jananishree0214-hash <jananishree0214@gmail.com>
2026-06-23 11:57:00 +0530

    Add files via upload

---
+++ Tree/src/Tree.java
@@ -0,0 +1,81 @@
+import java.util.*;
+
+class Node {
+    int data;
+    Node left, right;
+
+    Node(int data) {
+        this.data = data;
+        left = right = null;
+    }
+}
+
+public class Tree {
+
+    Node root;
+
+    void inorder(Node node) {
+        if (node != null) {
+            inorder(node.left);
+            System.out.print(node.data + " ");
+            inorder(node.right);
+        }
+    }
+
+    void preorder(Node node) {
+        if (node != null) {
+            System.out.print(node.data + " ");
+            preorder(node.left);
+            preorder(node.right);
+        }
+    }
+
+    void postorder(Node node) {
+        if (node != null) {
+            postorder(node.left);
+            postorder(node.right);
+            System.out.print(node.data + " ");
+        }
+    }
+
+    void levelOrder() {
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+
+        while (!queue.isEmpty()) {
+            Node temp = queue.poll();
+            System.out.print(temp.data + " ");
+
+            if (temp.left != null)
+                queue.add(temp.left);
+
+            if (temp.right != null)
+                queue.add(temp.right);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Tree tree = new Tree();
+
+        tree.root = new Node(8);
+        tree.root.left = new Node(4);
+        tree.root.right = new Node(12);
+        tree.root.left.left = new Node(2);
+        tree.root.left.right = new Node(6);
+        tree.root.right.left = new Node(10);
+        tree.root.right.right = new Node(14);
+
+        System.out.print("Inorder: ");
+        tree.inorder(tree.root);
+
+        System.out.print("\nPreorder: ");
+        tree.preorder(tree.root);
+
+        System.out.print("\nPostorder: ");
+        tree.postorder(tree.root);
+
+        System.out.print("\nLevel Order: ");
+        tree.levelOrder();
+    }
+}
